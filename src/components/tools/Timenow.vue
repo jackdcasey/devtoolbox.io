a667705c89af502ca9bff963b0b60db2ee05fb49
Jack Casey <jackdcasey@gmail.com>
2020-06-09 00:31:43 -0700

    Setup timenow components

---
+++ src/components/tools/Timenow.vue
@@ -0,0 +1,88 @@
+<template>
+  <b-container class="flex-container">
+  <b-row class="justify-content-md-center">
+    <b-col md="auto">
+        <div id="title">
+            <h1>Time Now</h1>
+            <h3>Current time in multiple formats</h3>
+        </div>
+
+        <div id="tooloutput" class="m-2">
+            <b-card class="m-2" title="ISO8601">
+                <OneLineWithCopy :text=isotime />
+            </b-card>
+            <b-card class="m-2" title="Ticks">
+                <OneLineWithCopy :text=tickstime />
+            </b-card>
+            <b-card class="m-2" title="Unix">
+                <OneLineWithCopy :text=unixtime />
+            </b-card>
+        </div>
+    </b-col>
+  </b-row>
+
+  </b-container>
+</template>
+
+<script>
+
+import OneLineWithCopy from '../utility/OneLineWithCopy'
+
+export default {
+  name: 'Timenow',
+  components: {
+      OneLineWithCopy
+  },
+  data: function () {
+    return {
+        isotime: "",
+        tickstime: "",
+        unixtime: "",
+        timer: '',
+        }
+    },
+    created () {
+        this.timer = setInterval(this.updatetime, 20)
+    },
+    methods: {
+        updatetime() {
+            var currentTime = new Date();
+            this.isotime = currentTime.toISOString();
+            this.tickstime = (currentTime.getTime() * 10000) + 621355968000000000 + Math.floor(Math.random() * 10000);
+            this.unixtime = Math.floor(new Date().getTime() / 1000);
+        }
+    },
+    beforeDestroy () {
+        clearInterval(this.timer)
+    }
+}
+</script>
+
+<style scoped>
+
+#title {
+    text-align: center;
+    margin-bottom: 50px;
+}
+
+#tooloutput {
+    width: 400px;
+    margin-top: 100px;
+    border: 2px solid rgba(0,0,0,.1);
+    border-radius: 10px;
+}
+
+.mono {
+    font-family:'Courier New', Courier, monospace;
+    font-weight: bold;
+    background-color: #ffffff;
+}
+
+.flex-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+}
+
+</style>
---
+++ src/components/utility/OneLineWithCopy.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <b-input-group>
+            <b-form-input readonly type="text" v-model="text" class="output"></b-form-input>
+            <b-input-group-append>
+            <b-button :variant=buttonvariant @click="copy"> {{buttontext}} </b-button>
+            </b-input-group-append>
+        </b-input-group>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OneLineWithCopy',
+    props: ['text'],
+    data: function () {
+        return {
+            buttontext: '',
+            buttonvariant: ''
+        }
+    },
+    beforeMount: function () {
+        this.initButton()
+    },
+    methods: {
+        copy() {
+            this.copyStringToClipboard(this.text)
+            this.buttontext = "Copied!"
+            this.buttonvariant = 'outline-success'
+
+            setTimeout(this.initButton, 2000);
+
+        },
+        initButton() {
+            this.buttontext = 'Copy!';
+            this.buttonvariant = 'outline-primary';
+        },
+        copyStringToClipboard(str) {
+            console.log("Copying to clipboard: " + str)
+            var el = document.createElement('textarea');
+            el.value = str;
+            el.setAttribute('readonly', '');
+            el.style = {position: 'absolute', left: '-9999px'};
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.output {
+    font-family:'Courier New', Courier, monospace;
+    font-weight: bold;
+    background-color: #ffffff;
+}
+
+</style>

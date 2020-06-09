a7edc22ef5014db6b36afb167d0b03973ddbea90
Jack Casey <jackdcasey@gmail.com>
2020-06-09 10:24:16 -0700

    Add build config

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'us-west-2',
+      bucket: 'devtoolbox.io',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
+}
